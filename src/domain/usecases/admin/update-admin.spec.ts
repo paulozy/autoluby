c914a156aef2323720a36e1d14aa4154f5d0b0a2
Paulo Abreu <paulo.santana@luby.software>
2023-01-05 17:17:24 -0300

    test: 🧪 create salesman entity test

---
+++ src/domain/usecases/admin/update-admin.spec.ts
@@ -0,0 +1,78 @@
+import { InMemoryUserRepository } from "@tests/repositories/in-memory-user-repository";
+import { CreateAdminUseCase } from "./create-admin";
+import { UpdateAdminUseCase } from "./update-admin";
+
+describe("Update Admin", () => {
+  test("should be able to update an admin on success", async () => {
+    const inMemoryUserRepository = new InMemoryUserRepository();
+    const createAdminUseCase = new CreateAdminUseCase(inMemoryUserRepository);
+    const updateAdminUseCase = new UpdateAdminUseCase(inMemoryUserRepository);
+
+    const { admin } = await createAdminUseCase.execute({
+      cpf: "12345678910",
+      name: "John Doe",
+      email: "john_doe.admin@email.com",
+      password: "121334",
+      bio: "I'm a admin",
+    });
+
+    const { admin: updatedAdmin } = await updateAdminUseCase.execute({
+      id: admin.id,
+      bio: "I'm the master admin",
+    });
+
+    expect(updatedAdmin).toBeTruthy();
+    expect(updatedAdmin).toEqual(
+      expect.objectContaining({
+        id: admin.id,
+        cpf: admin.cpf,
+        name: admin.name,
+        email: admin.email,
+        password: admin.password,
+        bio: "I'm the master admin",
+        permissions: admin.permissions,
+      })
+    );
+  });
+
+  test("should not be able to update an admin if it does not exists", async () => {
+    const inMemoryUserRepository = new InMemoryUserRepository();
+    const updateAdminUseCase = new UpdateAdminUseCase(inMemoryUserRepository);
+
+    await expect(
+      updateAdminUseCase.execute({
+        id: "non-existing-id",
+        bio: "I'm the master admin",
+      })
+    ).rejects.toThrow(new Error("User not found"));
+  });
+
+  test("should not be able to update an admin if the email is already in use", async () => {
+    const inMemoryUserRepository = new InMemoryUserRepository();
+    const createAdminUseCase = new CreateAdminUseCase(inMemoryUserRepository);
+    const updateAdminUseCase = new UpdateAdminUseCase(inMemoryUserRepository);
+
+    await createAdminUseCase.execute({
+      cpf: "12345678910",
+      name: "John Doe",
+      email: "john_doe.admin@email.com",
+      password: "121334",
+      bio: "I'm a admin",
+    });
+
+    const { admin } = await createAdminUseCase.execute({
+      cpf: "12345678910",
+      name: "John Doe",
+      email: "john_doe.admin@example.com",
+      password: "121334",
+      bio: "I'm a admin",
+    });
+
+    await expect(
+      updateAdminUseCase.execute({
+        id: admin.id,
+        email: "john_doe.admin@email.com",
+      })
+    ).rejects.toThrow(new Error("User already exists"));
+  });
+});
