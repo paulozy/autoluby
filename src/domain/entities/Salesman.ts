2259f3e34cfa1a47b293e304ad52f8dbf6d4d7c6
Paulo Abreu <paulo.santana@luby.software>
2023-01-05 17:17:10 -0300

    feat: ✨create salesman entity

--- src/domain/entities/Salesman.ts
+++ src/domain/entities/Salesman.ts
@@ -1,64 +1,21 @@
-import { randomUUID } from "node:crypto";
-import { Replace } from "src/utils/Replace";
+import { permissions } from "@utils/Permissions";
+import { Replace } from "@utils/Replace";
+import { IUserBaseProps, UserBase } from "./User";
 
-interface ISalesmanProps {
-  name: string;
-  cpf: string;
-  email: string;
-  bio: string;
-  password: string;
-  createdAt: Date;
-}
-
-export class Salesman {
-  private _id: string;
-  private props: ISalesmanProps;
-
+export class Salesman extends UserBase {
   constructor(
-    props: Replace<ISalesmanProps, { createdAt?: Date }>,
+    props: Replace<
+      IUserBaseProps,
+      { createdAt?: Date; permissions?: string[] }
+    >,
     id?: string
   ) {
-    this._id = id ?? randomUUID();
-    this.props = { ...props, createdAt: props.createdAt ?? new Date() };
-  }
+    super(props, id);
 
-  public get id(): string {
-    return this._id;
-  }
-
-  public get name(): string {
-    return this.props.name;
-  }
-
-  public set name(name: string) {
-    this.props.name = name;
-  }
-
-  public get cpf(): string {
-    return this.props.cpf;
-  }
-
-  public set cpf(cpf: string) {
-    this.props.cpf = cpf;
-  }
-
-  public get email(): string {
-    return this.props.email;
-  }
-
-  public set email(email: string) {
-    this.props.email = email;
-  }
-
-  public get bio(): string {
-    return this.props.bio;
-  }
-
-  public set bio(bio: string) {
-    this.props.bio = bio;
-  }
-
-  public set password(password: string) {
-    this.props.password = password;
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+      permissions: props.permissions ?? permissions.salesman,
+    };
   }
 }
