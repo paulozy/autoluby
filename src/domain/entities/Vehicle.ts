ef00f5b280781c73fdc8f30627d577a7abbd9919
Paulo Abreu <paulo.santana@luby.software>
2023-01-05 17:17:53 -0300

    feat: ✨ create vehicle entity

---
+++ src/domain/entities/Vehicle.ts
@@ -0,0 +1,77 @@
+import { Replace } from "@utils/Replace";
+
+interface IVehicleProps {
+  brand: string;
+  model: string;
+  year: number;
+  km: number;
+  color: string;
+  chassi: string;
+  price: number;
+  status: "available" | "sold" | "reserved";
+  aquiredIn: Date;
+}
+
+export class Vehicle {
+  private _id: string;
+  private props: IVehicleProps;
+
+  constructor(
+    props: Replace<
+      IVehicleProps,
+      { aquiredIn?: Date; status?: "available" | "sold" | "reserved" }
+    >,
+    id?: string
+  ) {
+    this._id = id || "";
+    this.props = {
+      ...props,
+      aquiredIn: props.aquiredIn || new Date(),
+      status: props.status || "available",
+    };
+  }
+
+  public get id(): string {
+    return this._id;
+  }
+
+  public get brand(): string {
+    return this.props.brand;
+  }
+
+  public get model(): string {
+    return this.props.model;
+  }
+
+  public get year(): number {
+    return this.props.year;
+  }
+
+  public get km(): number {
+    return this.props.km;
+  }
+
+  public get color(): string {
+    return this.props.color;
+  }
+
+  public get chassi(): string {
+    return this.props.chassi;
+  }
+
+  public get price(): number {
+    return this.props.price;
+  }
+
+  public get status(): "available" | "sold" | "reserved" {
+    return this.props.status;
+  }
+
+  public set status(status: "available" | "sold" | "reserved") {
+    this.props.status = status;
+  }
+
+  public get aquiredIn(): Date {
+    return this.props.aquiredIn;
+  }
+}
