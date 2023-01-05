897ab7d7c58a5eff92c32a9474939e5b5d02d31a
Paulo Abreu <paulo.santana@luby.software>
2023-01-05 17:16:39 -0300

    feat: ✨create admin entity

---
+++ src/domain/entities/Admin.ts
@@ -0,0 +1,21 @@
+import { permissions } from "@utils/Permissions";
+import { Replace } from "@utils/Replace";
+import { IUserBaseProps, UserBase } from "./User";
+
+export class Admin extends UserBase {
+  constructor(
+    props: Replace<
+      IUserBaseProps,
+      { createdAt?: Date; permissions?: string[] }
+    >,
+    id?: string
+  ) {
+    super(props, id);
+
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+      permissions: props.permissions ?? permissions.administrator,
+    };
+  }
+}
