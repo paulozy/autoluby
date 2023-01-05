51ee0f40126fa46a0b5e5af12d717f9da514a4bf
Paulo Abreu <paulo.santana@luby.software>
2023-01-05 17:19:03 -0300

    feat: ✨ create permissions helper to create default permissions

---
+++ src/utils/Permissions.ts
@@ -0,0 +1,15 @@
+export const permissions = {
+  administrator: [
+    "create_salesman",
+    "read_salesman",
+    "update_salesman",
+    "delete_salesman",
+
+    "create_admin",
+    "read_admin",
+    "update_admin",
+    "delete_admin",
+  ],
+
+  salesman: ["read_salesman", "update_salesman"],
+};
