e54018b8656ecdd3a18fa0e3352ed7094c4a0990
Paulo Abreu <paulo.santana@luby.software>
2023-01-11 10:48:26 -0300

    feat: ✨ add implementation of update admin methods

--- tests/repositories/in-memory-user-repository.ts
+++ tests/repositories/in-memory-user-repository.ts
@@ -1,5 +1,8 @@
 import { UserBase } from "@src/domain/entities/User";
-import { IUserRepository } from "@src/domain/repositories/user-repository";
+import {
+  IFindAllParams,
+  IUserRepository,
+} from "@src/domain/repositories/user-repository";
 
 export class InMemoryUserRepository implements IUserRepository {
   public users: UserBase[] = [];
@@ -8,11 +11,47 @@ export class InMemoryUserRepository implements IUserRepository {
     this.users.push(user);
   }
 
+  async findAll(params: IFindAllParams): Promise<UserBase[]> {
+    const { orderBy, itemPerPage, itemPageKey } = params;
+
+    let users = this.users;
+
+    if (orderBy === "CREATED_ASC") {
+      users = users.sort(
+        (a, b) => a.props.createdAt.getTime() - b.props.createdAt.getTime()
+      );
+    } else if (orderBy === "CREATED_DESC") {
+      users = users.sort(
+        (a, b) => b.props.createdAt.getTime() - a.props.createdAt.getTime()
+      );
+    }
+
+    if (itemPerPage && !itemPageKey) {
+      users = users.slice(0, itemPerPage);
+    }
+
+    if (itemPerPage && itemPageKey) {
+      const itemPageKeyIndex = users.findIndex(
+        (user) => user.id === itemPageKey
+      );
+
+      users = users.slice(itemPageKeyIndex, itemPageKeyIndex + itemPerPage);
+    }
+
+    return users;
+  }
+
   async findByEmail(email: string): Promise<UserBase> {
     return this.users.find((user) => user.email === email);
   }
 
   async findById(id: string): Promise<UserBase> {
     return this.users.find((user) => user.id === id);
+  }
+
+  async delete(id: string): Promise<void> {
+    const userIndex = this.users.findIndex((user) => user.id === id);
+
+    this.users.splice(userIndex, 1);
   }
 }
