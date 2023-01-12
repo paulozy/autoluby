91105a161a56b8f11390c4bef7bccb8a079151a7
Paulo Abreu <paulo.santana@luby.software>
2023-01-12 16:59:08 -0300

    test: 🧪 create tests to create salesman usecase

---
+++ src/domain/usecases/salesman/create-salesman.spec.ts
@@ -0,0 +1,68 @@
+import { InMemoryUserRepository } from "@tests/repositories/in-memory-user-repository";
+import { CreateSalesmanUseCase } from "./create-salesman";
+
+describe("Create Salesman", () => {
+  test("should be able create a new salesman", async () => {
+    const userRepository = new InMemoryUserRepository();
+    const createSalesanUseCase = new CreateSalesmanUseCase(userRepository);
+
+    const { user } = await createSalesanUseCase.execute({
+      cpf: "12345678910",
+      name: "John Doe",
+      email: "john_doe.salesman@email.com",
+      password: "123456",
+      bio: "I'm a salesman",
+    });
+
+    expect(user).toBeTruthy();
+    expect(user.id).toBeTruthy();
+    expect(userRepository.users).toHaveLength(1);
+    expect(userRepository.users[0]).toEqual(user);
+  });
+
+  test("should not be able create a new salesman with an existing email", async () => {
+    const userRepository = new InMemoryUserRepository();
+    const createSalesanUseCase = new CreateSalesmanUseCase(userRepository);
+
+    await createSalesanUseCase.execute({
+      cpf: "12345678910",
+      name: "John Doe",
+      email: "john_doe.salesman@email.com",
+      password: "123456",
+      bio: "I'm a salesman",
+    });
+
+    expect(async () => {
+      await createSalesanUseCase.execute({
+        cpf: "12345678911",
+        name: "John Doe",
+        email: "john_doe.salesman@email.com",
+        password: "123456",
+        bio: "I'm a salesman",
+      });
+    }).rejects.toThrowError("User already exists");
+  });
+
+  test("should not be able create a new salesman with an existing cpf", async () => {
+    const userRepository = new InMemoryUserRepository();
+    const createSalesanUseCase = new CreateSalesmanUseCase(userRepository);
+
+    await createSalesanUseCase.execute({
+      cpf: "12345678910",
+      name: "John Doe",
+      email: "john_doe.salesman@email.com",
+      password: "123456",
+      bio: "I'm a salesman",
+    });
+
+    expect(async () => {
+      await createSalesanUseCase.execute({
+        cpf: "12345678910",
+        name: "John Doe",
+        email: "john_doe.salesman@mail.com",
+        password: "123456",
+        bio: "I'm a salesman",
+      });
+    }).rejects.toThrowError("User already exists");
+  });
+});
