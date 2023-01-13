c27119a717c1b8b1911f923c427fbb6b6b850b45
Paulo Abreu <paulo.santana@luby.software>
2023-01-13 11:50:22 -0300

    test: 🧪 ensure that is possible aquire a new vehicle with correct values

---
+++ src/domain/usecases/vehicle/aquire-vehicle.spec.ts
@@ -0,0 +1,38 @@
+import { InMemoryVehicleRepository } from "@tests/repositories/in-memory-vehicle-repository";
+import { AquireVehicleUseCase } from "./aquire-vehicle";
+
+interface SutTypes {
+  inMemoryVehicleRepository: InMemoryVehicleRepository;
+  sut: AquireVehicleUseCase;
+}
+
+const makeSut = () => {
+  const inMemoryVehicleRepository = new InMemoryVehicleRepository();
+  const aquireVehicleUseCase = new AquireVehicleUseCase(
+    inMemoryVehicleRepository
+  );
+
+  return { inMemoryVehicleRepository, sut: aquireVehicleUseCase };
+};
+
+describe("Aquire vehicle", () => {
+  test("should be able aquire a new vehicle on success", async () => {
+    const { sut, inMemoryVehicleRepository } = makeSut();
+
+    const { vehicle } = await sut.execute({
+      brand: "Fiat",
+      model: "Uno",
+      year: 1998,
+      km: 100000,
+      color: "white",
+      chassi: "123456789",
+      price: 10000,
+    });
+
+    expect(vehicle).toHaveProperty("id");
+    expect(vehicle.aquiredIn).toBeInstanceOf(Date);
+    expect(vehicle.status).toBe("available");
+
+    expect(inMemoryVehicleRepository.vehicles).toHaveLength(1);
+  });
+});
