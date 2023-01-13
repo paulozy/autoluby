27712a2b1f5a5e1d313a3753f1a10f92abbc6e76
Paulo Abreu <paulo.santana@luby.software>
2023-01-13 12:03:31 -0300

    feat: ✨ add listAll vehicles method on contract and implementation

--- src/domain/repositories/vehicle-repository.ts
+++ src/domain/repositories/vehicle-repository.ts
@@ -1,5 +1,13 @@
 import { Vehicle } from "../entities/Vehicle";
 
+export interface IFindAllParams {
+  orderBy?: "AQUIRED_ASC" | "AQUIRED_DESC";
+  status?: "available" | "sold" | "reserved";
+  itemPerPage?: number;
+  itemPageKey?: string;
+}
+
 export interface IVehicleRepository {
   aquireVehicle(vehicle: Vehicle): Promise<void>;
+  listAll(params: IFindAllParams): Promise<Vehicle[]>;
 }
--- tests/repositories/in-memory-vehicle-repository.ts
+++ tests/repositories/in-memory-vehicle-repository.ts
@@ -1,10 +1,52 @@
 import { Vehicle } from "@src/domain/entities/Vehicle";
-import { IVehicleRepository } from "@src/domain/repositories/vehicle-repository";
+import {
+  IFindAllParams,
+  IVehicleRepository,
+} from "@src/domain/repositories/vehicle-repository";
 
 export class InMemoryVehicleRepository implements IVehicleRepository {
   public vehicles: Vehicle[] = [];
 
   async aquireVehicle(vehicle: Vehicle): Promise<void> {
     this.vehicles.push(vehicle);
+  }
+
+  async listAll(params: IFindAllParams): Promise<Vehicle[]> {
+    const { orderBy, status, itemPerPage, itemPageKey } = params;
+
+    let vehicles = this.vehicles;
+
+    if (status) {
+      vehicles = vehicles.filter((vehicle) => vehicle.status === status);
+    }
+
+    if (orderBy === "AQUIRED_ASC") {
+      vehicles = vehicles.sort(
+        (a, b) => a.aquiredIn.getTime() - b.aquiredIn.getTime()
+      );
+    }
+
+    if (orderBy === "AQUIRED_DESC") {
+      vehicles = vehicles.sort(
+        (a, b) => b.aquiredIn.getTime() - a.aquiredIn.getTime()
+      );
+    }
+
+    if (itemPerPage && !itemPageKey) {
+      vehicles = vehicles.slice(0, itemPerPage);
+    }
+
+    if (itemPerPage && itemPageKey) {
+      const itemPageKeyIndex = vehicles.findIndex(
+        (vehicle) => vehicle.id === itemPageKey
+      );
+
+      vehicles = vehicles.slice(
+        itemPageKeyIndex,
+        itemPageKeyIndex + itemPerPage
+      );
+    }
+
+    return vehicles;
   }
 }
