8d1196f7e7c86a2d5b3f8ba55533799155ff46b9
Paulo Abreu <paulo.santana@luby.software>
2023-01-13 11:54:35 -0300

    test: 🧪 ensure thats be able aquire a new vehicle with a custom aquiredIn date

--- src/domain/usecases/vehicle/aquire-vehicle.spec.ts
+++ src/domain/usecases/vehicle/aquire-vehicle.spec.ts
@@ -35,4 +35,28 @@ describe("Aquire vehicle", () => {
 
     expect(inMemoryVehicleRepository.vehicles).toHaveLength(1);
   });
+
+  test("should be able aquire a new vehicle with a custom aquiredIn date", async () => {
+    const { sut, inMemoryVehicleRepository } = makeSut();
+
+    const { vehicle } = await sut.execute({
+      brand: "Fiat",
+      model: "Uno",
+      year: 1998,
+      km: 100000,
+      color: "white",
+      chassi: "123456789",
+      price: 10000,
+      aquiredIn: new Date("2020-01-01"),
+    });
+
+    console.log(vehicle);
+
+    expect(vehicle).toHaveProperty("id");
+    expect(vehicle.aquiredIn).toBeInstanceOf(Date);
+    expect(vehicle.aquiredIn).toEqual(new Date("2020-01-01"));
+    expect(vehicle.status).toBe("available");
+
+    expect(inMemoryVehicleRepository.vehicles).toHaveLength(1);
+  });
 });
