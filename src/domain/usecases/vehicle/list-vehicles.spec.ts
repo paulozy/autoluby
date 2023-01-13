44cf75f2358f581b32545b88ea696ceb9506373c
Paulo Abreu <paulo.santana@luby.software>
2023-01-13 13:30:17 -0300

    test: 🧪 ensure thats possible get vehicles paginated without key

--- src/domain/usecases/vehicle/list-vehicles.spec.ts
+++ src/domain/usecases/vehicle/list-vehicles.spec.ts
@@ -91,4 +91,24 @@ describe("List Vehicles", () => {
     expect(vehicles).toHaveLength(2);
     expect(vehicles[0].status).toEqual("sold");
   });
+
+  test("should be able get vehicles per page", async () => {
+    const { sut, aquireVehicleUseCase } = makeSut();
+
+    const { vehicle: vehicleOne } = await aquireVehicleUseCase.execute(
+      makeVehicle()
+    );
+    const { vehicle: vehicleTwo } = await aquireVehicleUseCase.execute(
+      makeVehicle()
+    );
+    await aquireVehicleUseCase.execute(makeVehicle());
+    await aquireVehicleUseCase.execute(makeVehicle());
+
+    const { vehicles } = await sut.execute({
+      itemPerPage: 2,
+    });
+
+    expect(vehicles).toHaveLength(2);
+    expect(vehicles).toEqual(expect.arrayContaining([vehicleOne, vehicleTwo]));
+  });
 });
