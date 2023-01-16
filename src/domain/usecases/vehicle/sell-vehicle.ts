4cee1c7c6bbcca945e0d9eeeb965b9fec8b1c1fe
Paulo Abreu <paulo.santana@luby.software>
2023-01-16 15:26:54 -0300

    feat: ✨ create sell-vehicle-usecase

---
+++ src/domain/usecases/vehicle/sell-vehicle.ts
@@ -0,0 +1,49 @@
+import { Transaction } from "@src/domain/entities/Transaction";
+import { ITransactionRepository } from "@src/domain/repositories/transaction-reository";
+import { IVehicleRepository } from "@src/domain/repositories/vehicle-repository";
+
+interface ISellVehicleRequest {
+  vehicleId: string;
+  salesmanId: string;
+}
+
+interface ISellVehicleResponse {
+  transaction: Transaction;
+}
+
+export class SellVehicleUseCase {
+  private type: "sale" | "reserve" = "sale";
+
+  constructor(
+    private readonly vehicleRepository: IVehicleRepository,
+    private readonly transactionRepository: ITransactionRepository
+  ) {}
+
+  async execute(request: ISellVehicleRequest): Promise<ISellVehicleResponse> {
+    const { salesmanId, vehicleId } = request;
+
+    const vehicle = await this.vehicleRepository.findById(request.vehicleId);
+
+    if (!vehicle) {
+      throw new Error("Vehicle not found");
+    }
+
+    if (vehicle.status !== "available") {
+      throw new Error("Vehicle is not available to sale");
+    }
+
+    const transaction = new Transaction({
+      type: this.type,
+      vehicleId,
+      salesmanId,
+    });
+
+    await this.transactionRepository.save(transaction);
+
+    vehicle.status = "sold";
+
+    await this.vehicleRepository.save(vehicle);
+
+    return { transaction };
+  }
+}
