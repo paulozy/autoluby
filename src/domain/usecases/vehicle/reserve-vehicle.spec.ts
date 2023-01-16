d4d33db21eab410a9d01ea000e03b506f0e544d0
Paulo Abreu <paulo.santana@luby.software>
2023-01-16 15:52:58 -0300

    test: 🧪 ensure thats possible reserve a vehicle with correct values

---
+++ src/domain/usecases/vehicle/reserve-vehicle.spec.ts
@@ -0,0 +1,51 @@
+import { makeVehicle } from "@tests/factories/vehicle-factory";
+import { InMemoryTransactionRepository } from "@tests/repositories/in-memory-transaction-repository";
+import { InMemoryVehicleRepository } from "@tests/repositories/in-memory-vehicle-repository";
+import { AquireVehicleUseCase } from "./aquire-vehicle";
+import { ReserveVehicleUseCase } from "./reserve-vehicle";
+
+interface SutTypes {
+  sut: ReserveVehicleUseCase;
+  aquireVehicleUseCase: AquireVehicleUseCase;
+  transactionRepository: InMemoryTransactionRepository;
+}
+
+const makeSut = (): SutTypes => {
+  const vehicleRepository = new InMemoryVehicleRepository();
+  const transactionRepository = new InMemoryTransactionRepository();
+  const aquireVehicleUseCase = new AquireVehicleUseCase(vehicleRepository);
+  const sut = new ReserveVehicleUseCase(
+    vehicleRepository,
+    transactionRepository
+  );
+
+  return {
+    sut,
+    aquireVehicleUseCase,
+    transactionRepository,
+  };
+};
+
+describe("Reserve vehicle", () => {
+  test("should be able reserve a vehile on success", async () => {
+    const { sut, aquireVehicleUseCase, transactionRepository } = makeSut();
+
+    const { vehicle } = await aquireVehicleUseCase.execute(makeVehicle());
+
+    expect(vehicle.status).toBe("available");
+
+    const { transaction } = await sut.execute({
+      vehicleId: vehicle.id,
+      salesmanId: "any_salesman_id",
+    });
+
+    expect(transactionRepository.transactions).toHaveLength(1);
+
+    expect(transaction).toHaveProperty("id");
+    expect(transaction.type).toBe("reserve");
+    expect(transaction.salesmanId).toBe("any_salesman_id");
+
+    expect(transaction.vehicleId).toBe(vehicle.id);
+    expect(vehicle.status).toBe("reserved");
+  });
+});
