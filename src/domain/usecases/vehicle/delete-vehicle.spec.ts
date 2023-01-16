34eddbcd7521cd35d280d42fa5d28e1ded06b505
Paulo Abreu <paulo.santana@luby.software>
2023-01-16 15:42:25 -0300

    test: 🧪 ensure thats be possible delete a vehicle on success

---
+++ src/domain/usecases/vehicle/delete-vehicle.spec.ts
@@ -0,0 +1,36 @@
+import { makeVehicle } from "@tests/factories/vehicle-factory";
+import { InMemoryVehicleRepository } from "@tests/repositories/in-memory-vehicle-repository";
+import { AquireVehicleUseCase } from "./aquire-vehicle";
+import { DeleteVehicleUseCase } from "./delete-vehicle";
+
+interface SutTypes {
+  sut: DeleteVehicleUseCase;
+  aquireVehicleUseCase: AquireVehicleUseCase;
+  vehicleRepository: InMemoryVehicleRepository;
+}
+
+const makeSut = (): SutTypes => {
+  const vehicleRepository = new InMemoryVehicleRepository();
+  const aquireVehicleUseCase = new AquireVehicleUseCase(vehicleRepository);
+  const sut = new DeleteVehicleUseCase(vehicleRepository);
+
+  return {
+    sut,
+    aquireVehicleUseCase,
+    vehicleRepository,
+  };
+};
+
+describe("Delete vehicle", () => {
+  test("should be able delete a vehicle on success", async () => {
+    const { sut, aquireVehicleUseCase, vehicleRepository } = makeSut();
+
+    const { vehicle } = await aquireVehicleUseCase.execute(makeVehicle());
+
+    expect(vehicleRepository.vehicles).toHaveLength(1);
+
+    await sut.execute(vehicle.id);
+
+    expect(vehicleRepository.vehicles).toHaveLength(0);
+  });
+});
